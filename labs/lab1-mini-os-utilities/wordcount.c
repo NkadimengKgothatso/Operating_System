08fdeb5f55afa6dfbdfb0f5910b2be66af8695ac
NkadimengKgothatso <kgothatsonkadimeng15@gmail.com>
2026-08-08 18:32:54 +0200

    Refactor wordcount.c to improve argument handling and usage message

--- labs/lab1-mini-os-utilities/wordcount.c
+++ labs/lab1-mini-os-utilities/wordcount.c
@@ -3,27 +3,21 @@
 #include <string.h>
 
 int main(int argc, char *argv[]) {
-
-    if (argc != 2) {
-        printf("Usage: %s <string>\n", argv[0]);
-        return 1;
-    }
-
     char buffer[1024];
 
-    strncpy(buffer, argv[1], sizeof(buffer) - 1);
-    buffer[sizeof(buffer) - 1] = '\0';
+    if (argc >= 2) {
+        strncpy(buffer, argv[1], sizeof(buffer) - 1);
+        buffer[sizeof(buffer) - 1] = '\0';
+    } else {
+        buffer[0] = '\0'; // no argument -> empty string
+    }
 
     int count = 0;
-
     char *token = strtok(buffer, " ");
-
     while (token != NULL) {
         count++;
         token = strtok(NULL, " ");
     }
-
     printf("%d\n", count);
-
     return 0;
 }
